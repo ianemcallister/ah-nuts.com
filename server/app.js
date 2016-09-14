f247ca81329ff720ccb078c5e282f069106ad419
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-09-13 23:56:56 -0700

    feat: Adding Server and Serving Folder

---
+++ server/app.js
@@ -0,0 +1,29 @@
+
+//declare dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//define the app
+var app = express();
+
+//define the port
+var port = process.env.PORT || 3000;
+
+//get the URL encoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
+//tell it the folder to serve
+app.use(express.static('dist'));
+
+//define our body parsers
+app.use(jsonParser); // for parsing application/json
+app.use(urlencodedParser); // for parsing application/x-www-form-urlencoded
+
+//open the port for local development
+app.listen(port,function() {
+	console.log('Express server is up and running on port ' + port);
+});
+
+//export the module
+module.exports = app;
