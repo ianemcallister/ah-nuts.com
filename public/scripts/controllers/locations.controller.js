a19c8bea33c91097b2d8e2edc814998acbc0f570
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-11-23 09:10:53 -0800

    feat: Adding Region Finder Functionality.

--- public/scripts/controllers/locations.controller.js
+++ public/scripts/controllers/locations.controller.js
@@ -2,18 +2,92 @@ angular
     .module('ahNutsWebApp')
     .controller('locationsController', locationsController);
 
-locationsController.$inject = ['$log', '$routeParams'];
+locationsController.$inject = ['$log', '$routeParams', '$location'];
 
 /* @ngInject */
-function locationsController($log, $routeParams) {
+function locationsController($log, $routeParams, $location) {
 
 	//define view model variable
 	var vm = this;
+
+	vm.selectedState = $routeParams.state;
+	vm.statesObject = {
+		"CA": "California",
+		"OR": "Oregon",
+		"UT": "Utah"
+	}
+
+	vm.state = {
+		regionFinder: {
+			visable: true
+		},
+		locationLabel: {
+			visable: false
+		},		
+		locationsToday: {
+			visable: false
+		},
+		locationsThisWeek: {
+			visable: false
+		},
+		locationsChronoView: {
+			visable: false
+		}
+	}
 
 	//notify the params for the time being	TODO: TAKE THIS OUT LATER
 	$log.info('params are: ', $routeParams);
 
 	//local methods
+	function setRegion() {
+		//if there is a region, don't need the location finder
+		vm.state.regionFinder.visable = false;
+		vm.state.locationLabel.visable = true;
+		vm.state.locationsToday.visable = true;
+		vm.state.locationsThisWeek.visable = true;
+		vm.state.locationsChronoView.visable = true;
+
+	}
+
+	function init() {
+
+		//check for paramaters
+		//is there a state param?
+		if(typeof $routeParams.state != 'undefined') {
+
+			$log.info('found a state');
+
+			setRegion();
+
+			//is there a region?
+			if(typeof $routeParams.region != 'undefined') {
+				$log.info('found a region');
+			} else { 
+				$log.info('NO region');
+			}
+
+		} else {
+			$log.info('NO state');
+		}
+
+	}
 
 	//view methods
+	vm.locationRedirect = function(state, region) {
+		
+		var fullPath = 'locations/';
+
+		//notify the user fo the state
+		$log.info('got this, state.id:', state);
+		//$log.info('got this, regionID:', regionID);
+
+		//build the path
+		fullPath = fullPath + state.id;
+
+		//redirect
+		$location.path(fullPath);
+	}
+
+	//init
+	init();
 }
---
+++ public/scripts/directives/regionFinder.directive.js
@@ -0,0 +1,64 @@
+/* ToolBar.directive.js */
+
+/**
+* @desc toolbar directive that is used on the main page across the entire app.
+* @example <div tool-bar></div>
+*/
+
+angular
+	.module('ahNutsWebApp')
+	.directive('regionFinder', regionFinder);
+
+/* @ngInject */
+function regionFinder() {
+	var directive = {
+		restrict: 'AECM',
+		templateUrl: 'views/directives/regionFinder.directive.htm',
+		replace: true,
+		scope: {
+			locationRedirect: '&'
+		},
+		link: linkFunc,
+		controller: regionFinderController,
+		controllerAs: 'vm',
+		bindToController: true
+	}
+
+	/* @ngInject */
+	function linkFunc(scope, el, attr, ctrl) {
+    }
+
+    regionFinderController.$inject = ['$scope', '$log'];
+    /* @ngInject */
+    function regionFinderController($scope, $log) {
+	    var vm = this;
+
+	    vm.states = [
+	    	{ 
+	    		"name": "California",
+	    		"id": "CA"
+	    	},
+	    	{
+	    	 	"name": "Oregon",
+	    	 	"id": "OR"
+	    	},
+	    	{
+	    		"name": "Utah",
+	    		"id": "UT"
+	    	}
+	    ];
+	    	
+
+	    //view model methods
+	    vm.selectState = function(id) {
+	    	$log.info('got this', vm.states[id]);
+
+	    	//pass the id back up to the locations controller
+	    	vm.locationRedirect()(vm.states[id]);
+	    }
+
+	}
+
+	return  directive;
+		
+};
